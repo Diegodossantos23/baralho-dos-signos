0ee4c1eeeb231f450d350d3f700d2f992b9217a1
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-17 10:27:57 -0300

    estilizei o componente header

--- baralhosignos/src/components/Header/Header.js
+++ baralhosignos/src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { HeaderConainer } from './HeaderStyled';
 
 const Header = () => {
   return (
-    <div>
+    <HeaderConainer>
         <h1>Baralho dos Signos</h1>
-    </div>
+    </HeaderConainer>
   );
 }
 
