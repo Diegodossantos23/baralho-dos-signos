158a870f8b8295af74392f5544a2a32a4b533ebb
Diegodossantos23 <diegodossantos900@outlook.com>
2021-02-17 09:41:29 -0300

    importei o card signos e o header na homepage

--- baralhosignos/src/screens/HomePage/HomePage.js
+++ baralhosignos/src/screens/HomePage/HomePage.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import CardSignos from '../../components/CardSigno/CardSigno'
+import Header from '../../components/Header/Header'
 
 const HomePage = () => {
   return (
     <div>
-        HomePage
+        <Header/>
+        <div>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+            <CardSignos/>
+        </div>
     </div>
   );
 }
